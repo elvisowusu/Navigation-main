b6f073dfffaa56301cb0f4b88a9385da4bea9721
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-14 15:18:42 +0000

    added more styles

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,16 +5,16 @@ import logo from '../assets/logo.svg'
 export const NavBar =()=>{
     return(
         <nav className="flex justify-between items-center py-[1.5rem] px-[2.7rem] text-[1.3rem]">
-            <div className="flex gap-[4rem] items-center justify-center">
+            <div className="flex gap-[4rem] items-start">
             <img src={logo} width={110}/>
-            <div className="flex gap-[3rem] justify-center items-center">
+            <div className="flex gap-[3rem]">
                 <button>Features</button>
                 <button>Company</button>
                 <button>Careers</button>
                 <button>About</button>
             </div>
             </div>
-            <div className="flex gap-[2rem]">
+            <div className="flex gap-[2.8rem]">
                 <button>Login</button>
                 <button className="border outline-none border-AlmostBlack px-[1.5rem] py-[0.5rem] rounded-2xl">Register</button>
             </div>
