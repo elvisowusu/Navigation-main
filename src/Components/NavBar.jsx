5ae88ef3df4f537f97701ca14beca4b9c1086869
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-30 23:24:38 +0000

    Add arrowUp and arrowDown icons to NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,8 @@ import React,{useEffect, useReducer, useState} from "react";
 import logo from '../assets/logo.svg'
 import menu from '../assets/icon-menu.svg'
 import close from '../assets/icon-close-menu.svg'
+import arrowUp from '../assets/icon-arrow-up.svg'
+import arrowDown from '../assets/icon-arrow-down.svg'
 
 export const NavBar =()=>{
 
@@ -14,8 +16,6 @@ export const NavBar =()=>{
      } 
     })
     return(
-        <div>
-            
             <nav className={`flex justify-between items-center ${screenWidth > 768 ? `py-[1.5rem]`:`py-[1.3rem]`} ${screenWidth > 768 ? `px-[2.7rem]`:`px-[1rem]`} text-[1.3rem]`}>
                 <div className="flex gap-[4rem] items-start">
                 <img src={logo} width={screenWidth > 768? 110:80}/>
@@ -42,7 +42,7 @@ export const NavBar =()=>{
                     <>
                     <div className="fixed right-0 bg-gray-400 opacity-70 w-full h-[192vh] z-10"></div>
                     <div className="fixed right-0 bg-white w-2/3 h-[192vh] z-20 pt-[4rem] px-4"> 
-                        <div className="mt-[40rem] h-[9rem] w-[4rem]"> 
+                        <div className="mt-[40rem] flex flex-col items-start"> 
                             <NavContent/>
                         </div>
                         <div className="flex flex-col items-center mt-[3rem] gap-4">
@@ -54,7 +54,6 @@ export const NavBar =()=>{
                     :null
                 }
             </nav>
-        </div>
     )
 }
 
@@ -62,10 +61,10 @@ export const NavBar =()=>{
 
 
 
-export function NavContent(width) {
+export function NavContent() {
     return (
         <>
-         <button>Features</button>
+         <button>Features <span> <img src={arrowDown} alt="" /> </span> </button>
          <button>Company</button>
          <button>Careers</button>
          <button>About</button>    
