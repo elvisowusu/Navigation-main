2945c1b4608cb1b8ce74de63c7c8637705d67f4c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-30 01:50:02 +0000

    Add Body component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { NavBar } from './Components/NavBar'
+import { Body } from './Components/Body'
 
 function App() {
 
   return (
     <div className='font-Epilogue'>
       <NavBar/>
+      <Body/>
     </div>
   )
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,20 +4,22 @@ import logo from '../assets/logo.svg'
 
 export const NavBar =()=>{
     return(
-        <nav className="flex justify-between items-center py-[1.5rem] px-[2.7rem] text-[1.3rem]">
-            <div className="flex gap-[4rem] items-start">
-            <img src={logo} width={110}/>
-            <div className="flex gap-[3rem]">
-                <button>Features</button>
-                <button>Company</button>
-                <button>Careers</button>
-                <button>About</button>
-            </div>
-            </div>
-            <div className="flex gap-[2.8rem]">
-                <button>Login</button>
-                <button className="border outline-none border-AlmostBlack px-[1.5rem] py-[0.5rem] rounded-2xl">Register</button>
-            </div>
-        </nav>
+        <div>
+            <nav className="flex justify-between items-center py-[1.5rem] px-[2.7rem] text-[1.3rem]">
+                <div className="flex gap-[4rem] items-start">
+                <img src={logo} width={110}/>
+                <div className="flex gap-[3rem]">
+                    <button>Features</button>
+                    <button>Company</button>
+                    <button>Careers</button>
+                    <button>About</button>
+                </div>
+                </div>
+                <div className="flex gap-[2.8rem]">
+                    <button>Login</button>
+                    <button className="border outline-none border-AlmostBlack px-[1.5rem] py-[0.5rem] rounded-2xl">Register</button>
+                </div>
+            </nav>
+        </div>
     )
 }
