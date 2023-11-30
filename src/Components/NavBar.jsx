e09e9e5ce889c6c51c446a114647f2e8bf8f3936
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-30 02:34:04 +0000

    Add menu icon and close icon to NavBar component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,12 @@
 import React,{useEffect, useReducer, useState} from "react";
 import logo from '../assets/logo.svg'
-
+import menu from '../assets/icon-menu.svg'
+import close from '../assets/icon-close-menu.svg'
 
 export const NavBar =()=>{
 
     const [screenWidth,setScreenWidth]=useState(window.innerWidth);
+    const [menuOpen,setMenuOpen]=useState(false);
     useEffect(()=>{
         window.addEventListener('resize',()=>setScreenWidth(window.innerWidth))
      return window.removeEventListener('resize',()=>setScreenWidth(window.innerWidth))
@@ -14,20 +16,27 @@ export const NavBar =()=>{
             
             <nav className="flex justify-between items-center py-[1.5rem] px-[2.7rem] text-[1.3rem]">
                 <div className="flex gap-[4rem] items-start">
-                <img src={logo} width={110}/>
+                <img src={logo} width={screenWidth > 768?110:80}/>
                 {
                     screenWidth>768?
                     <div className="flex gap-[3rem]">
-                    <button>Features</button>
-                    <button>Company</button>
-                    <button>Careers</button>
-                    <button>About</button>
-                </div>:null
+                        <button>Features</button>
+                        <button>Company</button>
+                        <button>Careers</button>
+                        <button>About</button>
+                   </div>:null
                 }
                 </div>
                 <div className="flex gap-[2.8rem]">
-                    <button>Login</button>
-                    <button className="border outline-none border-AlmostBlack px-[1.5rem] py-[0.5rem] rounded-2xl">Register</button>
+                    {
+                        screenWidth>768?
+                        <>
+                        <button>Login</button>
+                        <button className="border outline-none border-AlmostBlack px-[1.5rem] py-[0.5rem] rounded-2xl">Register</button>
+                        </>:<button onClick={()=>{setMenuOpen(!menuOpen)}}>
+                            {menuOpen ? <img src={close} width={25}/> : <img src={menu} width={25}/> }
+                        </button> 
+                    }
                 </div>
             </nav>
         </div>
