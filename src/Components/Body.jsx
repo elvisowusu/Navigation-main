d95f7cff5e8f38d4745df4a01365f187ef2820f0
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-02 02:17:18 +0000

    Update Body component styling

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -23,8 +23,8 @@ export const Body =()=>{
             <div>{state.width > 768 ? <img src={desktop} alt="destop img"/>:<img src={mobile} alt="destop img"/>}</div>
             <div className="flex flex-col items-center">
                 <h1 className="text-[2.5rem] md:text-left font-extrabold text-AlmostBlack">Make remote work</h1>
-                <p className="text-center md:text-left text-[1.2rem] w-[21rem]">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-                <button className="outline-none text-AlmostWhite hover:text-AlmostBlack bg-AlmostBlack hover:bg-AlmostWhite px-[1rem] py-[0.5rem] rounded-xl text-sm md:text-base">Learn more</button>
+                <p className="text-center md:text-left text-[1.2rem] w-[22rem]">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+                <button className="outline-none text-AlmostWhite hover:text-AlmostBlack bg-AlmostBlack hover:bg-AlmostWhite px-[1.5rem] py-[0.6rem] rounded-xl text-sm md:text-base">Learn more</button>
             </div>
         </div>
     )
