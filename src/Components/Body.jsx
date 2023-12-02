84421d5fe0511a7865b9dccbd93b0130a21458aa
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-02 04:09:35 +0000

    Refactor Body component layout

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -28,13 +28,13 @@ export const Body =()=>{
     const image =[databiz,audiophile,meet,maker]
 
     return(
-        <div>
-            <div>{state.width > 1024 ? <img src={desktop} alt="destop img"/>:<img className="md:w-full" src={mobile} alt="destop img"/>}</div>
-            <div className="flex flex-col items-center">
-                <h1 className={`text-[2.5rem] sm:text-[5rem] md:text-[6.5rem] lg:text-left font-extrabold text-AlmostBlack mt-[2rem] sm:mt-[1.5rem]`}>Make remote work</h1>
+        <div className="flex flex-col lg:flex-row-reverse lg:justify-center lg:items-center lg:h-[89vh]">
+            <div className="lg:w-[30rem]">{state.width > 1024 ? <img src={desktop} alt="destop img"/>:<img className="md:w-full" src={mobile} alt="destop img"/>}</div>
+            <div className="flex flex-col items-center lg:items-start">
+                <h1 className={`text-[2.5rem] sm:text-[5rem] md:text-[6.5rem] font-extrabold text-AlmostBlack mt-[2rem] sm:mt-[1.5rem]`}>Make remote work</h1>
                 <p className={`text-center lg:text-left text-MediumGray text-[1.1rem] sm:text-[1.7rem] md:text-[2rem] w-[21rem] sm:w-[29rem] md:w-[44rem] mt-[1rem] sm:mt-[0.7rem]`}>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
                 <button className={`bodybtn`}>Learn more</button>
-                <div className={`flex items-center justify-between w-[22rem] sm:w-[36rem] ${state.width > 1024 ? `md:w-[30rem]`:`md:w-[50rem]`}  mt-[2rem] sm:mt-[1.5rem] sm:mb-[2rem]`}>
+                <div className={`flex items-center justify-between w-[22rem] sm:w-[36rem] md:w-[50rem] lg:w-[30rem] mt-[2rem] sm:mt-[1.5rem] sm:mb-[2rem]`}>
                     {
                         image.map((image,key)=>{
                             return <img className="w-[4rem] sm:w-[5rem]" key={key} src={image} alt="icon" />
