8d227291b093e1116c828f12425d82ff17c75f96
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-02 01:45:27 +0000

    Add reducer for handling screen width

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,10 +1,24 @@
+import { useReducer } from "react"
 
 
+const reducer=(state,action)=>{
+    switch(action.type){
+        case 'screenWidth':
+            return {...state,width:window.innerWidth};
+        default:
+            throw new Error();
+    }
+}
 
 export const Body =()=>{
+
+    const [state,dispatch]=useReducer(reducer,{width: window.innerWidth});
+
+
     return(
         <div className="h-screen">
-            
+            <div></div>
+            <div><h1>Make remote work</h1></div>
         </div>
     )
 }
