2e79da4ccff226b7dc624285148f13c2201c1172
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-02 02:38:49 +0000

    Update Body component styling

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -30,10 +30,10 @@ export const Body =()=>{
         <div className="h-screen">
             <div>{state.width > 768 ? <img src={desktop} alt="destop img"/>:<img src={mobile} alt="destop img"/>}</div>
             <div className="flex flex-col items-center">
-                <h1 className="text-[2.5rem] md:text-left font-extrabold text-AlmostBlack">Make remote work</h1>
-                <p className="text-center md:text-left text-MediumGray text-[1.1rem] w-[21rem]">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-                <button className="hover:border hover:border-AlmostBlack outline-none text-AlmostWhite hover:text-AlmostBlack bg-AlmostBlack hover:bg-AlmostWhite px-[1.5rem] py-[0.6rem] rounded-xl text-sm md:text-base">Learn more</button>
-                <div className="flex items-center justify-between w-[22rem]">
+                <h1 className="text-[2.5rem] md:text-left font-extrabold text-AlmostBlack mt-[2rem]">Make remote work</h1>
+                <p className="text-center md:text-left text-MediumGray text-[1.1rem] w-[21rem] mt-[1rem]">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+                <button className="hover:border hover:border-AlmostBlack outline-none text-AlmostWhite hover:text-AlmostBlack bg-AlmostBlack hover:bg-AlmostWhite px-[1.5rem] py-[0.6rem] rounded-xl text-sm md:text-base mt-[1rem]">Learn more</button>
+                <div className="flex items-center justify-between w-[22rem] mt-[2rem]">
                     {
                         image.map((image,key)=>{
                             return <img className="w-[4rem]" key={key} src={image} alt="icon" />
