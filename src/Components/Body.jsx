853d039e973c1d55803e29c8742db4565491c410
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-02 02:35:03 +0000

    Add client logos to Body component

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useReducer } from "react"
 import mobile from '../assets/image-hero-mobile.png'
 import desktop from '../assets/image-hero-desktop.png'
+import audiophile from '../assets/client-audiophile.svg'
+import databiz from '../assets/client-databiz.svg'
+import maker from '../assets/client-maker.svg'
+import meet from '../assets/client-meet.svg'
+
+
 
 const reducer=(state,action)=>{
     switch(action.type){
@@ -18,6 +24,8 @@ export const Body =()=>{
         dispatch({type:'screenWidth'})
     },[state.width])
 
+    const image =[databiz,audiophile,meet,maker]
+
     return(
         <div className="h-screen">
             <div>{state.width > 768 ? <img src={desktop} alt="destop img"/>:<img src={mobile} alt="destop img"/>}</div>
@@ -25,8 +33,12 @@ export const Body =()=>{
                 <h1 className="text-[2.5rem] md:text-left font-extrabold text-AlmostBlack">Make remote work</h1>
                 <p className="text-center md:text-left text-MediumGray text-[1.1rem] w-[21rem]">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
                 <button className="hover:border hover:border-AlmostBlack outline-none text-AlmostWhite hover:text-AlmostBlack bg-AlmostBlack hover:bg-AlmostWhite px-[1.5rem] py-[0.6rem] rounded-xl text-sm md:text-base">Learn more</button>
-                <div>
-
+                <div className="flex items-center justify-between w-[22rem]">
+                    {
+                        image.map((image,key)=>{
+                            return <img className="w-[4rem]" key={key} src={image} alt="icon" />
+                        })
+                    }
                 </div>
             </div>
         </div>
