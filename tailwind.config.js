7709665bf6069aa7b1421766b0c5729bbabc5180
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-04 23:43:05 +0000

    Fix responsive layout in Body component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -17,5 +21,5 @@ export default {
     },
   },
   plugins: [],
-}
+};
 
